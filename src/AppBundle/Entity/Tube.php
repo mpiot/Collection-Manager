e8d881a41dfe3f968b26d825336aafec34555e42
Mathieu Piot <mathieu.piot@agroparistech.fr>
2016-09-13 14:56:58 +0200

    Change method that calculate freeSpace in box, and store cell name in database.

--- src/AppBundle/Entity/Tube.php
+++ src/AppBundle/Entity/Tube.php
@@ -45,6 +45,13 @@ class Tube
     private $cell;
 
     /**
+     * @var int
+     *
+     * @ORM\Column(name="cellName", type="string", length=255)
+     */
+    private $cellName;
+
+    /**
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255, unique=true)
@@ -123,6 +130,17 @@ class Tube
 
     public function setBox(Box $box)
     {
+        // If the actualbox is null, it's a new tube
+        if(null === $this->box) {
+            // Then we remove one space in the new box
+            $box->setFreeSpace($box->getFreeSpace() - 1);
+        // Else if the actual box is not null and different to the new box
+        } elseif (null !== $this->box && $this->box !== $box) {
+            // Then we add a free space in the previous box, and remove on in the new box
+            $box->setFreeSpace($box->getFreeSpace() - 1);
+            $this->box->setFreeSpace($this->box->getFreeSpace() + 1);
+        }
+
         $this->box = $box;
 
         return $this;
@@ -145,11 +163,19 @@ class Tube
         return $this->cell;
     }
 
+    public function setCellName($cellName)
+    {
+        $this->cellName = $cellName;
+    }
+
+    public function getCellName()
+    {
+        return $this->cellName;
+    }
+
     public function setName($name)
     {
         $this->name = $name;
-
-        return $this;
     }
 
     public function getName()
@@ -193,21 +219,6 @@ class Tube
         return $this->deleted;
     }
 
-    public function getCellName()
-    {
-        $availableLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-        $cellsName = [];
-        for ($i = 0; $i < $this->box->getRowNumber(); $i++) {
-            for ($j = 0; $j < $this->box->getColNumber(); $j++) {
-                $cellsName[] = $availableLetters[$i].($j+1);
-            }
-        }
-
-        return $cellsName[$this->cell];
-    }
-
-
     /**
      * Before persist.
      *
@@ -243,8 +254,25 @@ class Tube
 
         // Generate the tube name
         $this->name = $projectPrefix.'_'.$boxLetter.$boxCell.$lastLetter;
+    }
 
-        // When add a tube, inform the box to remove a cell in the freeSpace
-        $this->box->tubeAllocation();
+    /**
+     * Before persist.
+     *
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function autoCellName()
+    {
+        $availableLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+        $cellsName = [];
+        for ($i = 0; $i < $this->box->getRowNumber(); $i++) {
+            for ($j = 0; $j < $this->box->getColNumber(); $j++) {
+                $cellsName[] = $availableLetters[$i].($j+1);
+            }
+        }
+
+        $this->cellName = $cellsName[$this->cell];
     }
 }
