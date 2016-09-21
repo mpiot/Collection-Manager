d7b082e87a38695b855b455841e6ff4da9d4ac3a
Mathieu Piot <mathieu.piot@agroparistech.fr>
2016-09-21 09:01:33 +0200

    ShowHide Js function on advanced search results.

---
+++ web/js/showHide.js
@@ -0,0 +1,22 @@
+function showHide(linkContainer, container) {
+
+    var $lessLink=$('<a><span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span></a>');
+    var $moreLink=$('<a><span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></a>');
+
+    linkContainer.append($lessLink, $moreLink);
+    linkContainer.find($moreLink).hide();
+
+    $lessLink.click(function(e) {
+        e.preventDefault();
+        container.slideUp();
+        $lessLink.hide();
+        $moreLink.show();
+    });
+
+    $moreLink.click(function(e) {
+        e.preventDefault();
+        container.slideDown();
+        $moreLink.hide();
+        $lessLink.show();
+    });
+}
