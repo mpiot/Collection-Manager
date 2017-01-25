b49d45e4a930249d71d5a42f3e41fd9b9db32717
Mathieu <math.piot@gmail.com>
2017-01-25 14:29:22 +0000

    Apply fixes from StyleCI

--- src/AppBundle/Controller/BiologicalOriginCategoryController.php
+++ src/AppBundle/Controller/BiologicalOriginCategoryController.php
@@ -83,14 +83,14 @@ class BiologicalOriginCategoryController extends Controller
                 'attr' => [
                     'data-btn-group' => 'btn-group',
                     'data-btn-position' => 'btn-first',
-                ]
+                ],
             ])
             ->add('saveAndAdd', SubmitType::class, [
                 'label' => 'Create and Add',
                 'attr' => [
                     'data-btn-group' => 'btn-group',
                     'data-btn-position' => 'btn-last',
-                ]
+                ],
             ]);
 
         $form->handleRequest($request);
--- src/AppBundle/Controller/SpeciesController.php
+++ src/AppBundle/Controller/SpeciesController.php
@@ -85,14 +85,14 @@ class SpeciesController extends Controller
                 'attr' => [
                     'data-btn-group' => 'btn-group',
                     'data-btn-position' => 'btn-first',
-                ]
+                ],
             ])
             ->add('saveAndAdd', SubmitType::class, [
                 'label' => 'Create and Add',
                 'attr' => [
                     'data-btn-group' => 'btn-group',
                     'data-btn-position' => 'btn-last',
-                ]
+                ],
             ]);
 
         $form->handleRequest($request);
