2c20d9a0277c3e61d56bc6f7ee28f8f2116272f6
Mathieu Piot <mathieu.piot@inra.fr>
2017-03-29 15:33:35 +0200

    Manager all javascripts file with Grunt and Bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,36 @@ module.exports = function(grunt) {
                 files: ['web/js/*.js'],
                 tasks: ['javascript']
             }
+        },
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'bower_components/jquery/dist/',
+                        dest: 'web/built',
+                        src: 'jquery.min.js'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/jquery-ui/',
+                        dest: 'web/built',
+                        src: 'jquery-ui.min.js'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap/dist/js/',
+                        dest: 'web/built',
+                        src: 'bootstrap.min.js'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/select2/dist/js/',
+                        dest: 'web/built',
+                        src: 'select2.min.js'
+                    }
+                ]
+            }
         }
     });
 
@@ -47,4 +77,5 @@ module.exports = function(grunt) {
     grunt.registerTask('default', ['css','javascript']);
     grunt.registerTask('css', ['cssmin']);
     grunt.registerTask('javascript', ['uglify']);
+    grunt.registerTask('cp', ['copy']);
 };
