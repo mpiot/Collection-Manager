fd069ffb386baeb71db035745b2e0f378200b227
Mathieu Piot <mathieu.piot@inra.fr>
2017-11-06 07:31:29 +0100

    Manager assets with WebPack encore

---
+++ assets/js/images.js
@@ -0,0 +1,5 @@
+require('../images/403.png');
+require('../images/404.png');
+require('../images/500.png');
+require('../images/ajax-loader.gif');
+require('../images/icon.png');
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('web/build/')
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .autoProvidejQuery()
+    .autoProvideVariables({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+    })
+    .enableVersioning()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableSassLoader(function(sassOptions) {}, {
+        resolveUrlLoader: false
+    })
+    .createSharedEntry('js/vendor', ['jquery', 'bootstrap-sass'])
+    .addEntry('js/app', [
+        './assets/js/add-entity-on-fly.js',
+        './assets/js/advanced-search.js',
+        './assets/js/anti-iframe.js',
+        './assets/js/charmap.js',
+        './assets/js/collection-type.js',
+        './assets/js/delay.js',
+        './assets/js/field_autocomplete.js',
+        './assets/js/images.js',
+        './assets/js/modal-confirmation-message.js',
+        './assets/js/plasmid-add-gbk-file.js',
+        './assets/js/quick-search.js',
+        './assets/js/select2-conf.js',
+        './assets/js/strain-tubes-dynamic.js'
+    ])
+    .addStyleEntry('css/app', ['./assets/scss/app.scss'])
+;
+
+module.exports = Encore.getWebpackConfig();
