836837140d74fe5a95c7f4a9245d97af419d9150
Mathieu Piot <mathieu.piot@inra.fr>
2018-01-12 13:41:22 +0100

    [Controller][Security] Remove Service call for AuthenticationUtil

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
@@ -13,14 +14,12 @@ class SecurityController extends Controller
      * @Route("/login", name="login")
      * @Method({"GET", "POST"})
      */
-    public function loginAction(Request $request)
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
         if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
             // redirect authenticated users to homepage
             return $this->redirect($this->generateUrl('homepage'));
         }
-
-        $authenticationUtils = $this->get('security.authentication_utils');
 
         //get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
