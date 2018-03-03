d9630f82d02c664c05f4458e8c23fc622aa76147
Mathieu Piot <math.piot@gmail.com>
2018-03-03 12:52:06 +0100

    [Docker][Entrypoint] Test if MySQL is available to handle connections

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,32 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
 # if parameters.yml, doesn't exists, it's the first container execution
 if [ ! -f /var/www/html/app/config/parameters.yml ]; then
-    # Wait other containers are ready
-    sleep 10
+    # Create parameters.yml file, to avoid doctrine:migrations test bug
+    touch /var/www/html/app/config/parameters.yml
+
+    # Test if the database is ready to handle connections
+    statusCode=1
+    i=0
+    while [ $statusCode -ne 0 ]; do
+        i=$[$i+1]
+	sleepTime=$(( $i * 5 ))
+
+        if [ $sleepTime -gt 30 ]; then
+            set -e
+        fi
+
+        bin/console doctrine:migrations:status
+        statusCode=$?
+
+        if [ $statusCode -ne 0  ]; then
+            echo "Wait $sleepTime seconds..."
+            sleep $sleepTime
+	fi
+    done
+
+    # Exit script if there is an error
+    set -e
 
     # Create log file
     touch var/logs/$SYMFONY_ENV.log
